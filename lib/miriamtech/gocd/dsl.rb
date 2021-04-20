da39f5ce33577b964dcef40199bac05d737cfb6b
Ken Treis <ken@miriamtech.com>
2021-04-20 16:42:17 -0700

    Fix incorrect `Object.extend` in dsl.rb
    This was a mistake I made when trying to correct Rubocop offenses.

--- lib/miriamtech/gocd/dsl.rb
+++ lib/miriamtech/gocd/dsl.rb
@@ -5,4 +5,10 @@ require_relative 'lambda'
 require_relative 'paths'
 require_relative 'tasks'
 
-Object.extend MiriamTech::GoCD::DSL
+# Add our DSL methods to the top level namespace without polluting the Object inheritance tree.
+# This technique comes from `rake`, see e.g.
+# https://github.com/ruby/rake/blob/11973e8d31f29aee2e40d874206c9240956f86ed/lib/rake/dsl_definition.rb#L195
+#
+# rubocop:disable Style/MixinUsage
+extend MiriamTech::GoCD::DSL
+# rubocop:enable Style/MixinUsage
