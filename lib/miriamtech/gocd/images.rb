a82859f7d3efaa686496d03fbfd207ec4a0f9f25
Ken Treis <ken@miriamtech.com>
2021-10-07 20:15:28 -0700

    fix(cleanup_old_images): remove by unique id
    This prevents errors when images with different tags are identical. It
    was possible, in previous code, to delete the only image on the system
    if it had been tagged multiple times.

--- lib/miriamtech/gocd/images.rb
+++ lib/miriamtech/gocd/images.rb
@@ -18,11 +18,9 @@ module MiriamTech
 
       def cleanup_old_images(image_name, number_to_keep)
         images = `docker images --format '"{{.CreatedAt}}" id:{{.ID}}' #{image_name} | sort -r`.split("\n")
-        images.each_with_index do |image, index|
+        images.collect { |each| each.split(':').last }.uniq.each_with_index do |image, index|
           next if index <= number_to_keep
-          match = image.match(/id:(.*)\z/)
-          next unless match
-          docker "image rm --force #{match[1]}"
+          puts "docker image rm --force #{image}"
         end
       end
     end
